fa4e1dfbfe99ccf1d7c618073cede3d3d2f5fb73
Enrique Garcia <garcia@lapp.in2p3.fr>
2022-04-05 13:27:49 +0200

    add material for python and jupyter lecture on 05 April 2022

---
+++ 20220405_python_jupyter_material/compute_volume.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from math import pi
+
+if __name__ == '__main__':
+    r = 1.5
+    vol = 4 / 3 * pi * r**3
+
+    print('The volume of a sphere with radius %f m is %f m**3' %(r,  vol))
+
